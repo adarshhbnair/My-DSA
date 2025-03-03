4faf75570b323454d0b33eaa72cb9c9c1984052a
adarshhbnair <adarshhbnair@gmail.com>
2025-03-03 20:30:47 +0530

    sliding window

---
+++ 121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <limits.h> // For INT_MAX
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int minPrice = INT_MAX; 
+        int maxProfit = 0; 
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "Maximum profit: " << solution.maxProfit(prices) << endl;
+    return 0;
+}

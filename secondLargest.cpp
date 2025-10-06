5902ab9609642f72421b9064a82c35a11acffe88
adarshhbnair <adarshhbnair@gmail.com>
2025-10-06 20:56:07 +0530

    Arrays

--- secondLargest.cpp
+++ secondLargest.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+int getSecondLargest(vector<int> &arr){
+    int n = arr.size();
+    int largest = -1, secondLargest = -1;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]>largest){
+            secondLargest = largest;
+            largest=arr[i];
+        }
+        else if(arr[i]<largest && arr[i]>secondLargest){
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
 int main(){
-    
+    vector<int> arr = {1,2,3,4,5,6};
+    cout<<getSecondLargest(arr);
+    return 0;
 }

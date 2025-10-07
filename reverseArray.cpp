57a5fbeded9a5b05acc8295e871fdd4622b64b3a
adarshhbnair <adarshhbnair@gmail.com>
2025-10-07 11:46:13 +0530

    Arrays - Two-pointers

---
+++ reverseArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//using two-pointers approach
+void reverseArray(vector<int> &arr){
+    int left=0, right=arr.size()-1;
+    while(left<right){
+        swap(arr[left],arr[right]);
+        left++;
+        right--;
+    }
+}
+int main(){
+    vector<int> arr = {1,2,3,4,5};
+    reverseArray(arr);
+    for(int i = 0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
